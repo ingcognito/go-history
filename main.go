9da19bfb527d2cf63ed84a20d9546e163ced0007
noah-ing <ing.noah@gmail.com>
2019-10-07 19:15:53 -0400

    counting git works

--- main.go
+++ main.go
@@ -1,17 +1,66 @@
 package main
 
 import (
-	"log"
+	"bufio"
+	"fmt"
 	"os"
-	"os/exec"
+	"strings"
 )
 
+type histFile struct {
+	line     int
+	commands string
+}
+
 func main() {
-	cmd := exec.Command("ls", "-lah")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+	fmt.Printf("hello world")
+	readHistFile()
+}
+
+func getHistFile() {
+	//Get Histfile by using environment variable
+	//Allow user to enter their own histfile they would like analyzed
+
+	histfile, err := os.Open("./hist.txt")
+	check(err)
+	defer histfile.Close()
+	fmt.Println("file successfully opened", histfile)
+
+}
+
+func readHistFile() {
+
+	file, err := os.Open("./hist.txt")
+	check(err)
+	defer file.Close()
+
+	var lines []string
+	var git int
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	//this allows to read the lines, line by line, this is a ghetto analyzer but can pump out stats
+	fmt.Println("read lines:")
+	for _, line := range lines {
+		if strings.Contains(line, "git") {
+			git++
+		}
+	}
+	fmt.Println("there are this many lines:", len(lines))
+	fmt.Println("first line: ", lines[0])
+	fmt.Println("you used git this many times: ", git)
+	// most used binary
+	// most used languages
+	// date of of first terminal usage
+	//
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
